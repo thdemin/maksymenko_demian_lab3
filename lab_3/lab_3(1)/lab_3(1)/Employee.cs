4e59483531346c1a34b8358860ba56dbbfbcc43f
Demian <103683248+thdemin@users.noreply.github.com>
2022-06-22 14:07:54 +0400

    duck

---
+++ lab_3/lab_3(1)/lab_3(1)/Employee.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace lab_3_1_
+{
+    public abstract class Employee
+    {
+        public string Name { get; set; }         
+
+        public abstract string GetWorkerInfo();
+
+        public override bool Equals(object obj)
+        {
+            return GetHashCode() == obj.GetHashCode();
+        }
+
+        public override int GetHashCode()
+        {
+            return HashCode.Combine(Name);
+        }
+
+        public override string ToString()
+        {
+            return GetWorkerInfo();
+        }
+    }
+
+}
---
+++ lab_3/lab_3(1)/lab_3(1)/Trainee.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace lab_3_1_
+{
+    public class Trainee : Employee     //наслідує властивості працівника
+    {
+        public override string GetWorkerInfo()
+        {
+            Console.WriteLine("Стажер");
+            return Name;
+        }
+    }
+}
